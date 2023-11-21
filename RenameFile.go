bfe3cdfc3ab9411cfa585ecc032b838158fc1509
李洪岩 <5000521@qq.com>
2023-11-21 09:23:17 +0800

    修正命名

---
+++ RenameFile.go
@@ -0,0 +1,23 @@
+package main
+
+import "os"
+
+type RenameFile struct {
+	fileName   string
+	oldIpaName string
+}
+
+func NewRenameFile(fileName string, oldIpaName string) *RenameFile {
+	return &RenameFile{
+		oldIpaName: oldIpaName,
+		fileName:   fileName,
+	}
+}
+func (m *RenameFile) Rename() error {
+	err := os.Rename(m.oldIpaName, m.fileName+".ipa")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	msTimestamp := time.Now().UnixNano() / int64(time.Millisecond)
 	timeStr := time.Unix(0, msTimestamp*int64(time.Millisecond)).Format("20060102150405")
 	fileName := fmt.Sprintf("%s%s", timeStr, GenerateRandomString(8))
+
 	ipaPath, err := GetIPAFilePath()
 	if err != nil {
 		fmt.Println("获取 IPA 文件路径失败:", err)
@@ -24,9 +25,9 @@ func main() {
 		fmt.Println("替换 Info.plist 失败:", err)
 	}
 
-	err = NewFileMover(fileName, ipaPath).Move()
+	err = NewRenameFile(fileName, ipaPath).Rename()
 	if err != nil {
-		fmt.Println("移动文件失败:", err)
+		fmt.Println("重命名文件失败:", err)
 	}
 
 	fmt.Println("运行完毕,回车关闭窗口!")
